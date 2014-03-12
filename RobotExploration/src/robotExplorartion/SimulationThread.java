48b2f73cf5d5435beb1659b32d5e31c6dea8a78a
Paul <paulmonk92@gmail.com>
2014-03-12 14:54:00 +0000

    Transfers simulation setings from the main menu to the simulation and runs it

--- RobotExploration/src/robotExplorartion/SimulationThread.java
+++ RobotExploration/src/robotExplorartion/SimulationThread.java
@@ -3,20 +3,33 @@ package robotExplorartion;
 /**
  * A thread which is created to run a robot exploration simulation
  * @author Paul Monk
- * @version 20/01/2014
+ * @version 12/03/2014
  */
 public class SimulationThread implements Runnable
-{
-	int threadNumber;
+{	
+	private Simulation simulation;
 	
-	public SimulationThread(int threadNumberIn)
+	/**
+	 * Sets up a thread to run a simulation
+	 * @param threadNumberIn The number of this thread (to distinguish between different simulations running at the same time)
+	 * @param proprietaryAlgorithmIn which exploration algorithm should be used (true for proprietary, false for frontier)
+	 * @param twoRobotsIn True if 2 robots are running in the simulation, false if just 1 robot is running
+	 * @param coordinatedIn True if the robots will coordinate with each other in the simulation, false otherwise
+	 * @param obstacleProbabilityIn The chance of obstacles occurring (0-100%)
+	 */
+	public SimulationThread(int threadNumberIn, boolean proprietaryAlgorithmIn, boolean twoRobotsIn,
+			boolean coordinatedIn, int obstacleProbabilityIn)
 	{
-		threadNumber = threadNumberIn;
+		simulation = new Simulation(threadNumberIn, proprietaryAlgorithmIn, twoRobotsIn,
+				coordinatedIn, obstacleProbabilityIn);
 	}//SimulationThread
 	
+	/**
+	 * Runs the thread, starting the simulation
+	 */
 	@Override
 	public void run()
 	{
-		new Simulation(threadNumber);
+		simulation.startSimulation();
 	}//run
 }//simulationThread
